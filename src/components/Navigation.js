6d078fa29c449af093f9141de483456f7e1e1601
valkim55 <vesper5521@gmail.com>
2023-01-02 15:42:06 -0500

    setup app base

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+
+const Navigation = () => {
+  return (
+    <nav>
+        <h3>About Us</h3>
+        <h3>Home</h3>
+        <h3>Login/Signup</h3>
+    </nav>
+  )
+}
+
+export default Navigation
