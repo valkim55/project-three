e899a6530cd235b310dda910414833fba8c4ae81
Caitlin O'Reilly <coreilly29@gmail.com>
2023-01-03 16:18:28 -0500

    restructed file organization in client and server directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
