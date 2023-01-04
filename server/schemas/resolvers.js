ffa7c9b4ba1670012f9ccf5ceee8de8b9681e7e6
Caitlin O'Reilly <coreilly29@gmail.com>
2023-01-03 19:26:25 -0500

    login and signup functionalty works

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,6 +18,7 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       try {
+        console.log(args);
         const user = await User.create(args);
         const token = signToken(user);
         return { token, user };
