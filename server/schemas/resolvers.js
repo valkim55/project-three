4afb7852179f0f8511b3542b7af61cb08fd62ea0
Caitlin O'Reilly <coreilly29@gmail.com>
2023-01-10 16:17:40 -0500

    merge files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,6 +40,30 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+
+    // saveProduct: async (parent, { productData }, context) => {
+    //   if (context.user) {
+    //     const updatedUser = await User.findOneAndUpdate(
+    //       { _id: context.user._id },
+    //       { $push: { savedProducts: productData } },
+    //       { new: true }
+    //     );
+    //     return updatedUser;
+    //   }
+    //   throw new AuthenticationError("You need to be logged in!");
+    // },
+
+    removeProduct: async (parent, { productId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedProducts: { productId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,6 +21,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    removeProduct(productId: ID!): User
   }
 `;
 
