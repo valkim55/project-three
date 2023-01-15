1861cd790b734faf4ff2551f5296add3f2f5205a
valkim55 <vesper5521@gmail.com>
2023-01-14 22:28:55 -0500

    deploying again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deploySkincare",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/deploySkincare",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
