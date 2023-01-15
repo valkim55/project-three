6438cad4a7bdd87647a4df7b2dd90aa5e21d703c
valkim55 <vesper5521@gmail.com>
2023-01-14 22:51:09 -0500

    deploying2

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
 app.get("/categories/:category", (req, res) => {
   console.log(req.params.category);
   //console.log(jsonData)
